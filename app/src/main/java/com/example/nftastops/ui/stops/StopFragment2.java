14c28db67ae768d22822dbaeaadb260f52f01c12
manali <manalips30@gmail.com>
2020-04-28 01:52:21 -0400

    Multiple select spinner

--- app/src/main/java/com/example/nftastops/ui/stops/StopFragment2.java
+++ app/src/main/java/com/example/nftastops/ui/stops/StopFragment2.java
@@ -18,6 +18,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageView;
@@ -93,7 +94,7 @@ public class StopFragment2 extends Fragment {
     private CheckBox timeTable;
     private CheckBox systemMap;
     private TextInputLayout comments;
-    private Spinner acroutes;
+    private MultiSpinner acroutes;
     private Button submitButton;
     StopTransactions stopTransactions;
     NetworkAPICall apiCAll;
@@ -153,7 +154,7 @@ public class StopFragment2 extends Fragment {
         timeTable = root.findViewById(R.id.timetableId);
         systemMap = root.findViewById(R.id.systemMapId);
         comments = root.findViewById(R.id.admin_comments);
-        MultiSpinner acroutes = (MultiSpinner) root.findViewById(R.id.autocomplete_route);
+        acroutes = root.findViewById(R.id.autocomplete_route);
 
 
         submitButton = root.findViewById(R.id.fragment2Next);
@@ -183,15 +184,18 @@ public class StopFragment2 extends Fragment {
         }
         routesDN.add(0, new Dropdowns("--Select--"));
 
-        //ArrayAdapter<Dropdowns> routesAdapter = new ArrayAdapter<>
-        //        (getActivity(), android.R.layout.simple_list_item_1, routesDN);
-        //acroutes.setAdapter(routesAdapter);
+//        ArrayAdapter<Dropdowns> routesAdapter = new ArrayAdapter<>
+//                (getActivity(), android.R.layout.simple_list_item_1, routesDN);
+//        acroutes.setAdapter(routesAdapter);
 
-        List<String> routeNames = new ArrayList<String>();
-        for (int i = 0; i < routesDN.size(); i++) {
-            routeNames.add(routesDN.get(i).getDisplay_name());
-        }
-        acroutes.setItems(routeNames);
+
+        acroutes.setItems(routesDN);
+
+//        List<String> routeNames = new ArrayList<String>();
+//        for (int i = 0; i < routesDN.size(); i++) {
+//            routeNames.add(routesDN.get(i).getDisplay_name());
+//        }
+        //acroutes.setItems(routeNames);
 
         String stopTransactionJson = getArguments().getString("stopTransaction");
         stopTransactions = gson.fromJson(stopTransactionJson, StopTransactions.class);
@@ -214,6 +218,9 @@ public class StopFragment2 extends Fragment {
             stopTransactions.setTime_table(timeTable.isChecked());
             stopTransactions.setSystem_map(systemMap.isChecked());
             stopTransactions.setAdmin_comments(comments.getEditText().getText().toString());
+
+            stopTransactions.setRoute((List<Dropdowns>) acroutes.getSelectedItems());
+
             stopTransactions.setStatus(Constants.INPROGRESS);
             if (stopTransactions.getRequest_id() != null) {
                 ServiceRequests serviceRequests = new ServiceRequests();
--- app/src/main/java/com/example/nftastops/utilclasses/MultiSpinner.java
+++ app/src/main/java/com/example/nftastops/utilclasses/MultiSpinner.java
@@ -10,7 +10,9 @@ import android.widget.SpinnerAdapter;
 import androidx.appcompat.widget.AppCompatSpinner;
 
 import com.example.nftastops.model.Dropdowns;
+import com.example.nftastops.ui.stops.StopFragment2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
@@ -19,16 +21,18 @@ public class MultiSpinner extends AppCompatSpinner implements
         DialogInterface.OnMultiChoiceClickListener
 {
 
-    String[] _items = null;
+    //String[] _items = null;
+    Dropdowns[] _items = null;
     boolean[] mSelection = null;
 
-    ArrayAdapter<String> simple_adapter;
+    ArrayAdapter<String> tough_adapter;
+    ArrayAdapter<Dropdowns> simple_adapter;
 
     public MultiSpinner(Context context)
     {
         super(context);
 
-        simple_adapter = new ArrayAdapter<String>(context,
+        simple_adapter = new ArrayAdapter<Dropdowns>(context,
                 android.R.layout.simple_spinner_item);
 
         super.setAdapter(simple_adapter);
@@ -37,7 +41,7 @@ public class MultiSpinner extends AppCompatSpinner implements
     public MultiSpinner(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        simple_adapter = new ArrayAdapter<String>(context,
+        simple_adapter = new ArrayAdapter<Dropdowns>(context,
                 android.R.layout.simple_spinner_item);
         super.setAdapter(simple_adapter);
     }
@@ -47,17 +51,28 @@ public class MultiSpinner extends AppCompatSpinner implements
             mSelection[which] = isChecked;
 
             simple_adapter.clear();
-            simple_adapter.add(buildSelectedItemString());
+            List<Dropdowns> dp = new ArrayList<>();
+            dp = buildSelectedItem();
+            for(Dropdowns item:dp){
+                simple_adapter.add(item);}
         } else {
             throw new IllegalArgumentException(
                     "Argument 'which' is out of bounds.");
         }
     }
 
+
     @Override
     public boolean performClick() {
         final AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-        builder.setMultiChoiceItems(_items, mSelection, this);
+        int len = _items.length;
+
+        String[] _itemsShow = new String [len];
+        for (int i = 0; i < len; i++){
+            _itemsShow[i] = _items[i].getDisplay_name();
+        }
+
+        builder.setMultiChoiceItems(_itemsShow, mSelection, this);
 
         builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
             @Override
@@ -74,25 +89,29 @@ public class MultiSpinner extends AppCompatSpinner implements
 
     @Override
     public void setAdapter(SpinnerAdapter adapter) {
-        throw new RuntimeException(
-                "setAdapter is not supported by MultiSelectSpinner.");
+//        throw new RuntimeException(
+//                "setAdapter is not supported by MultiSelectSpinner.");
+
+        super.setAdapter(simple_adapter);
     }
 
-    public void setItems(String[] items) {
-        _items = items;
+//    public void setItems(String[] items) {
+//        _items = items;
+//        mSelection = new boolean[_items.length];
+//        simple_adapter.clear();
+//        simple_adapter.add(_items[0]);
+//        Arrays.fill(mSelection, false);
+//    }
+
+
+    public void setItems(List<Dropdowns> items) {
+        _items = items.toArray(new Dropdowns[items.size()]);
         mSelection = new boolean[_items.length];
         simple_adapter.clear();
         simple_adapter.add(_items[0]);
         Arrays.fill(mSelection, false);
     }
 
-    public void setItems(List<String> items) {
-        _items = items.toArray(new String[items.size()]);
-        mSelection = new boolean[_items.length];
-        simple_adapter.clear();
-        simple_adapter.add(_items[0]);
-        Arrays.fill(mSelection, false);
-    }
 
     public void setSelection(String[] selection) {
         for (String cell : selection) {
@@ -102,21 +121,6 @@ public class MultiSpinner extends AppCompatSpinner implements
                 }
             }
         }
-    }
-
-    public void setSelection(List<String> selection) {
-        for (int i = 0; i < mSelection.length; i++) {
-            mSelection[i] = false;
-        }
-        for (String sel : selection) {
-            for (int j = 0; j < _items.length; ++j) {
-                if (_items[j].equals(sel)) {
-                    mSelection[j] = true;
-                }
-            }
-        }
-        simple_adapter.clear();
-        simple_adapter.add(buildSelectedItemString());
     }
 
     public void setSelection(int index) {
@@ -129,9 +133,30 @@ public class MultiSpinner extends AppCompatSpinner implements
             throw new IllegalArgumentException("Index " + index
                     + " is out of bounds.");
         }
-        simple_adapter.clear();
-        simple_adapter.add(buildSelectedItemString());
+        tough_adapter.clear();
+        tough_adapter.add(buildSelectedItemString());
     }
+
+
+    public void setSelection(List<Dropdowns> selection) {
+        for (int i = 0; i < mSelection.length; i++) {
+            mSelection[i] = false;
+        }
+        for (Dropdowns sel : selection) {
+            for (int j = 0; j < _items.length; ++j) {
+                if (_items[j].equals(sel)) {
+                    mSelection[j] = true;
+                }
+            }
+        }
+        simple_adapter.clear();
+
+        List<Dropdowns> dp = new ArrayList<>();
+        dp = buildSelectedItem();
+        for(Dropdowns item:dp){
+            simple_adapter.add(item);}
+    }
+
 
     public void setSelection(int[] selectedIndicies) {
         for (int i = 0; i < mSelection.length; i++) {
@@ -142,18 +167,34 @@ public class MultiSpinner extends AppCompatSpinner implements
                 mSelection[index] = true;
             } else {
                 throw new IllegalArgumentException("Index " + index
+                        //    public void setSelection(int index) {
+//        for (int i = 0; i < mSelection.length; i++) {
+//            mSelection[i] = false;
+//        }
+//        if (index >= 0 && index < mSelection.length) {
+//            mSelection[index] = true;
+//        } else {
+//            throw new IllegalArgumentException("Index " + index
+//                    + " is out of bounds.");
+//        }
+//        simple_adapter.clear();
+//        simple_adapter.add(buildSelectedItemString());
+//    }
                         + " is out of bounds.");
             }
         }
         simple_adapter.clear();
-        simple_adapter.add(buildSelectedItemString());
+        List<Dropdowns> dp = new ArrayList<>();
+        dp = buildSelectedItem();
+        for(Dropdowns item:dp){
+            simple_adapter.add(item);}
     }
 
     public List<String> getSelectedStrings() {
         List<String> selection = new LinkedList<String>();
         for (int i = 0; i < _items.length; ++i) {
             if (mSelection[i]) {
-                selection.add(_items[i]);
+                selection.add(_items[i].getDisplay_name());
             }
         }
         return selection;
@@ -180,10 +221,26 @@ public class MultiSpinner extends AppCompatSpinner implements
                 }
                 foundOne = true;
 
-                sb.append(_items[i]);
+                sb.append(_items[i].getDisplay_name());
             }
         }
         return sb.toString();
+    }
+
+    private List<Dropdowns> buildSelectedItem() {
+        boolean foundOne = false;
+        List<Dropdowns> dp = new ArrayList<>();
+        for (int i = 0; i < _items.length; ++i) {
+            if (mSelection[i]) {
+                if (foundOne) {
+                    //sb.append(", ");
+                }
+                foundOne = true;
+
+                dp.add(_items[i]);
+            }
+        }
+        return dp;
     }
 
     public String getSelectedItemsAsString() {
@@ -196,9 +253,24 @@ public class MultiSpinner extends AppCompatSpinner implements
                     sb.append(", ");
                 }
                 foundOne = true;
-                sb.append(_items[i]);
+                sb.append(_items[i].getDisplay_name());
             }
         }
         return sb.toString();
+    }
+
+    public List<Dropdowns> getSelectedItems() {
+        boolean foundOne = false;
+        List<Dropdowns> dp = new ArrayList<>();
+        for (int i = 0; i < _items.length; ++i) {
+            if (mSelection[i]) {
+                if (foundOne) {
+                    //sb.append(", ");
+                }
+                foundOne = true;
+                dp.add(_items[i]);
+            }
+        }
+        return dp;
     }
 }
